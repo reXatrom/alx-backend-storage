e063b5275377ae066fa4f5772ffdf8989e9e950f
reXatrom <ajstylejoshua2837@gmail.com>
2023-08-10 12:03:05 +0000

    0x00-MySQL_Advanced/3-glam_rock.sql (correction).

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,8 @@
--- Lists all bands with Glam rock as their style,
--- ranked by their longevity
--- Column names must be: band_name & lifespan
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
 
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT
+    band_name AS band_name,
+    IFNULL(split, CURRENT_DATE()) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
